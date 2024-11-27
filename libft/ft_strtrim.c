67546c8739e2f117fdca98d0cc7f552ce5ac13d8
lilia10010 <25871372+Lilia10010@users.noreply.github.com>
2024-11-27 20:59:31 -0300

    add libft

---
+++ libft/ft_strtrim.c
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strtrim.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lpaula-n <lpaula-n@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/26 16:00:37 by lpaula-n          #+#    #+#             */
+/*   Updated: 2024/11/03 05:02:17 by lpaula-n         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+static int	ft_isset(char c, const char *set)
+{
+	while (*set)
+		if (c == *set++)
+			return (1);
+	return (0);
+}
+
+char	*ft_strtrim(char const *s1, char const *set)
+{
+	char	*result;
+	char	*start;
+	char	*end;
+
+	if (!s1 || !set)
+		return (0);
+	start = (char *)s1;
+	end = start + ft_strlen(s1);
+	while (*start && ft_isset(*start, set))
+		++start;
+	while (start < end && ft_isset(*(end - 1), set))
+		--end;
+	result = ft_substr(start, 0, end - start);
+	return (result);
+}
