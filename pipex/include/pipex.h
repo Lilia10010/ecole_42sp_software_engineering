0d276a68eca64cbd7d97bf33196b3da2ff04d0ac
Lilia10010 <liliaxtz2@gmail.com>
2025-01-19 15:20:58 -0300

    wip

--- pipex/include/pipex.h
+++ pipex/include/pipex.h
@@ -10,9 +10,16 @@
 # include <fcntl.h>
 # include "../libft/libft.h"
 
-void	print_hello(void);
-void	print_error(const char *msg);
+// funções auxiliares (colocar na utils)
+void error_exit(const char *msg);
+void free_paths(char **paths);
+char *find_executable_path(char **paths, char *cmd);
+char *get_command_path(char *cmd, char **envp);
+void execute_command(char *cmd, char **envp);
 
-char    *get_command_path(char *command, char **envp);
+// funções principais
+void process_child(int *pipes, int prev_pipe, int file_in, int file_out, int is_last_command);
+void process_commands(int argc, char **argv, char **envp, int file_in, int file_out);
+void pipex(int argc, char **argv, char **envp);
 
 #endif
--- pipex/src/pipex.c
+++ pipex/src/pipex.c
@@ -6,72 +6,112 @@
 /*   By: microbiana <microbiana@student.42.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/13 23:19:25 by lpaula-n          #+#    #+#             */
-/*   Updated: 2025/01/17 18:12:16 by microbiana       ###   ########.fr       */
+/*   Updated: 2025/01/19 15:20:03 by microbiana       ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 
 #include "../include/pipex.h"
 
+//mover p utils
 void error_exit(const char *msg)
 {
+	printf("Error: %s\n", msg);
     perror(msg);
     exit(EXIT_FAILURE);
 }
 
-char *get_command_path(char *command, char **envp)
+void free_paths(char **paths)
 {
-    char *path_env;
-    char **paths;
+	int i;
+
+	i = 0;
+	while (paths[i])
+	{
+		free(paths[i]);
+		i++;
+	}
+	free(paths);
+}
+
+char *find_executable_path(char **paths, char *cmd)
+{
+    char *part_path;
     char *full_path;
-    size_t i;
+    int i;
 
-    while (*envp)
-    {
-        if (ft_strncmp(*envp, "PATH=", 5) == 0)
-        {
-            path_env = *envp + 5;
-            break;
-        }
-        ++envp;
-    }
-    if (!path_env)
-        return (NULL);
-
-    paths = ft_split(path_env, ':');
-    i = 0;
+	i = 0;
     while (paths[i])
     {
-        full_path = malloc(strlen(paths[i]) + strlen(command) + 2);
+        part_path = ft_strjoin(paths[i], "/");
+        if (!part_path)
+            return (NULL);
+
+        full_path = ft_strjoin(part_path, cmd);
+        free(part_path);
+
         if (!full_path)
             return (NULL);
-        sprintf(full_path, "%s/%s", paths[i], command);
-        if (access(full_path, X_OK) == 0)
-        {
-            while (paths[i])
-                free(paths[i++]);
-            free(paths);
+
+        if (access(full_path, F_OK) == 0) //x verifica se é executável f verifica a existência
             return (full_path);
-        }
+
         free(full_path);
-        i++;
+		++i;
     }
-    i = 0;
-    while (paths[i])
-        free(paths[i++]);
-    free(paths);
+
     return (NULL);
 }
+
+
+char *get_command_path(char *cmd, char **envp)
+{
+    char **paths;
+    char *path;
+    int i;
+
+    i = 0;
+    while (ft_strnstr(envp[i], "PATH", 4) == 0)
+        i++;
+
+    paths = ft_split(envp[i] + 5, ':');
+    if (!paths)
+        return (NULL);
+
+    path = find_executable_path(paths, cmd);
+    free_paths(paths);
+    return (path);
+}
+
+
 
 void execute_command(char *cmd, char **envp)
 {
     char **args = ft_split(cmd, ' ');
     char *path = get_command_path(args[0], envp);
     if (!path)
-        error_exit("Command not found");
+	{
+        error_exit("Command not found =>");
+
+	}
     execve(path, args, envp);
     perror("execve");
     exit(EXIT_FAILURE);
+}
+//daqui pra cima mover para utils 
+
+
+void process_child(int *pipes, int prev_pipe, int file_in, int file_out, int is_last_command)
+{
+	dup2(prev_pipe, STDIN_FILENO);
+	if (is_last_command)
+		dup2(file_out, STDOUT_FILENO);
+	else
+		dup2(pipes[1], STDOUT_FILENO);
+	close(pipes[0]);
+	close(pipes[1]);
+	if (prev_pipe != file_in)
+		close(prev_pipe);
 }
 
 void process_commands(int argc, char **argv, char **envp, int file_in, int file_out)
@@ -80,11 +120,13 @@ void process_commands(int argc, char **argv, char **envp, int file_in, int file_
 	int pipes[2];
 	pid_t pid;
 	int prev_pipe;
-	
+	int is_last_command;
+		
 	prev_pipe = file_in;
 	i = 0;
 	while (i < argc - 3)
-	{
+	{	
+		is_last_command = i == argc - 4;
 		if(pipe(pipes) < 0)
 			error_exit("Error: pipe");
 		pid = fork();
@@ -92,16 +134,7 @@ void process_commands(int argc, char **argv, char **envp, int file_in, int file_
 			error_exit("Error: fork");
 		if (pid == 0)
 		{
-			dup2(prev_pipe, STDIN_FILENO);
-			if (i < argc - 4)
-				dup2(pipes[1], STDOUT_FILENO);
-			else
-				dup2(file_out, STDOUT_FILENO);
-
-			close(pipes[0]);
-			close(pipes[1]);
-			if (prev_pipe != file_in)
-				close(prev_pipe);
+			process_child(pipes, prev_pipe, file_in, file_out, is_last_command);
 			execute_command(argv[i + 2], envp);
 		}
 		close(prev_pipe);
@@ -111,52 +144,21 @@ void process_commands(int argc, char **argv, char **envp, int file_in, int file_
 	}
 }
 
-void setup_pipex(int argc, char **argv, char **envp)
+void pipex(int argc, char **argv, char **envp)
 {
-    int file_in, file_out;
+    int file_in;
+	int file_out;
 
     file_in = open(argv[1], O_RDONLY);
     if (file_in < 0)
         error_exit("Error: open input file");
     file_out = open(argv[argc - 1], O_WRONLY | O_CREAT | O_TRUNC, 0666);
     if (file_out < 0)
-        error_exit("Error: open output file");
-
-    //para cada comando intermediário
-
-
+		error_exit("Error: open output file");
+	
 	process_commands(argc, argv, envp, file_in, file_out);
-    /* while(i < argc - 3)
-    {
-        if (pipe(pipes) < 0)
-            error_exit("Error: pipe");
-        pid = fork();
-        if (pid < 0)
-            error_exit("Error: fork");
-        if (pid == 0) // Child process
-        {
-            dup2(prev_pipe, STDIN_FILENO);
-            if (i < argc - 4)
-                dup2(pipes[1], STDOUT_FILENO); // se não for o último comando, redireciona a saída para o pipe
-            else
-                dup2(file_out, STDOUT_FILENO); //se for o ultimo comando, redireciona a saída para o arquivo
-
-            close(pipes[0]);
-            close(pipes[1]);
-            if (prev_pipe != file_in)
-                close(prev_pipe);
-
-            execute_command(argv[i + 2], envp);
-        }
-
-        close(prev_pipe);
-        close(pipes[1]);
-        prev_pipe = pipes[0]; // resetar p/ (leitura)
-		++i;
-    } */
-
-    // Wait for all child processes
-    while (wait(NULL) > 0);
+    
+    while (wait(NULL) > 0); //espera todos os processos
 }
 
 int main(int argc, char **argv, char **envp)
@@ -166,6 +168,6 @@ int main(int argc, char **argv, char **envp)
         write(2, "Usage: ./pipex infile cmd1 cmd2 ... cmdN outfile\n", 49);
         return (1);
     }
-    setup_pipex(argc, argv, envp);
+    pipex(argc, argv, envp);
     return (0);
 }
