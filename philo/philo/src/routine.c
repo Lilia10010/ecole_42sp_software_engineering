0277cebbd0ad90696866f2003f0f25bfe88d4812
Pedro da Costa e Silva Monteiro <83892026+peda-cos@users.noreply.github.com>
2025-05-15 00:02:22 -0300

    fix(routine): add running state check in sleep routine
    - Prevent printing thinking state after simulation stops
    - Add safety mutex lock to check running state

---
+++ philo/philo/src/routine.c
@@ -0,0 +1,41 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   routine.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: peda-cos <peda-cos@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/11 18:02:14 by microbiana        #+#    #+#             */
+/*   Updated: 2025/05/14 23:54:23 by peda-cos         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "philo.h"
+
+void	handle_sleep(t_Context *ctx, t_Philo *philo)
+{
+	int	is_running;
+
+	print_logs(SLEEPING, ctx, philo);
+	usleep(ctx->time_to_sleep * 1000);
+	pthread_mutex_lock(&ctx->running_lock);
+	is_running = ctx->running;
+	pthread_mutex_unlock(&ctx->running_lock);
+	if (is_running)
+	{
+		print_logs(THINKING, ctx, philo);
+		usleep(1000);
+	}
+}
+
+void	handle_eat(t_Context *ctx, t_Philo *philo)
+{
+	pthread_mutex_lock(&ctx->total_meals_lock);
+	ctx->total_meals++;
+	pthread_mutex_unlock(&ctx->total_meals_lock);
+	pthread_mutex_lock(&ctx->last_meal_lock);
+	philo->last_meal = get_time_ms(ctx);
+	pthread_mutex_unlock(&ctx->last_meal_lock);
+	print_logs(EATING, ctx, philo);
+	usleep(ctx->time_to_eat * 1000);
+}
